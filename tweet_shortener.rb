dac1f7ae0ef77e80db1b72d58c22141d731f96b4
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-17 02:00:19 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
-# Write your code here.
+require 'pry'
+def word_substituter(tweet)
+  dictionary = {
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
+
+  #   words = tweet.split(" ")
+  #
+  #   words.map{|word|
+  #     if dictionary[word]
+  #       dictionary[word]
+  #     else
+  #       word
+  #     end
+  #   }.join(" ")
+  #
+  # end
+
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+#if tweet is over 140 characters AFTER shortening
+#only display the first 130 characters and then show "..."
+
+def shortened_tweet_truncator(tweet)
+  if bulk_tweet_shortener(tweet).length > 140
+        puts "(word_substituter(tweet)[0...140]) + '...'"
+  elsif bulk_tweet_shortener(tweet).length < 140
+    tweet
+  end
+end
