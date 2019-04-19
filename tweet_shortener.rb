e0f0a29702e52cdab9cd4bd4fc29f61e1a864821
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-19 22:27:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -52,7 +52,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-        word_substituter(tweet)[0...136] + "..."
+        word_substituter(tweet)[0..136] + "..."
   elsif word_substituter(tweet).length < 140
     tweet
   end
